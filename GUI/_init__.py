4194e53484c294ceca7dce2e232fd9a933f44c84
TRS-09 <teo.reeson@gmail.com>
2026-07-17 11:13:07 +0100

    home page aesthetics

--- GUI/_init__.py
+++ GUI/_init__.py
@@ -18,9 +18,12 @@ stack.addWidget(setting)   # index 1
 stack.addWidget(csv)       # index 2
 
 # Navigation
-home.go_settings.connect(lambda: stack.setCurrentIndex(2))
+home.go_settings.connect(lambda: stack.setCurrentIndex(1))
+home.go_CSV.connect(lambda: stack.setCurrentIndex(2))
 setting.go_home.connect(lambda: stack.setCurrentIndex(0))
 csv.go_home.connect(lambda: stack.setCurrentIndex(0))
+
+
 
 # CSV → HomeScreen button update
 csv.csv_loaded.connect(home.updatestrategyButtonColor)
